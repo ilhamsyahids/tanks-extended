d7fbe5ad42330553bc5b8c9b1d5a0ee558309991
Fakhrurrida <fakhrurridawidodo@gmail.com>
2021-03-27 23:36:50 +0700

    add p1p2

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioScript : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectSlider;
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource[] backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+        if (firstPlayInt == 0)
+        {
+            backgroundFloat = .125f;
+            soundEffectsFloat = .75f;
+            backgroundSlider.value = backgroundFloat;
+            soundEffectSlider.value = soundEffectsFloat;
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+            soundEffectSlider.value = soundEffectsFloat;
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectSlider.value);
+    }
+
+    void OnAppliationFocus(bool inFocus)
+    {
+        if (!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void UpdateSound()
+    {
+        for (int h = 0; h < backgroundAudio.Length; h++)
+        {
+            backgroundAudio[h].volume = backgroundSlider.value;
+        }
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectSlider.value;
+        }
+    }
+
+}
---
+++ Assets/PrestartAudioSetting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PrestartAudioSetting : MonoBehaviour
+{
+
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource[] backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ContinueSettings();
+    }
+
+    private void ContinueSettings()
+    {
+        for (int h = 0; h < backgroundAudio.Length; h++)
+        {
+            backgroundAudio[h].volume = PlayerPrefs.GetFloat(BackgroundPref);
+        }
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = PlayerPrefs.GetFloat(SoundEffectsPref);
+        }
+    }
+}

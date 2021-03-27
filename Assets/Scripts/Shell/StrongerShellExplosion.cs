b574e163207098db982ca7d7d8861a305ca31137
Nathaniel Evan <nathanielevan@zohomail.com>
2021-03-27 21:28:56 +0700

    Makes new prefabs StrongerShell and StrongerTank, and StrongerShellExplosion script

---
+++ Assets/Scripts/Shell/StrongerShellExplosion.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mirror;
+
+public class StrongerShellExplosion : NetworkBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;
+    public AudioSource m_ExplosionAudio;
+    public float m_MaxDamage = 130f;
+    public float m_ExplosionForce = 1000f;
+    public float m_MaxLifeTime = 0.8f;
+    public float m_ExplosionRadius = 5f;
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), m_MaxLifeTime);
+    }
+
+    [Server]
+    public void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody) continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+            if (!targetHealth) continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+        RpcExplode();
+        gameObject.SetActive(false);
+    }
+
+    [ClientRpc]
+    void RpcExplode()
+    {
+        gameObject.SetActive(false);
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
